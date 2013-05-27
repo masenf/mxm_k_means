a7e779e3b78452526bc6db982bdf3a28406ca9fc
Masen Furer <furerm@students.wwu.edu>
2013-05-26 18:12:40 -0700

    core of get_tfidf is done

--- get_tfidf.py
+++ get_tfidf.py
@@ -1,46 +1,84 @@
-# get_tdidf.py
-# create a tdidf cooefficient for each row in the lyrics database
+# get_tfidf.py
+# create a tfidf cooefficient for each row in the lyrics database
 
 import sqlite3
+from IPython.core.debugger import Tracer; trace = Tracer()
+from math import log
 import sys
 
-class TDIDFCounter(object):
+class TFIDFCounter(object):
     def __init__(self, dbh):
         self.dbh = dbh
         self.words = {}
+        self.totaldocs = 0
+        self._init_totals_for_words()
+
     def _init_totals_for_words(self):
+        dbg("Initializing document frequency totals...")
         c = self.dbh.cursor()
         for row in c.execute("SELECT word, COUNT(track_id) FROM lyrics GROUP BY word"):
             self.words[row[0]] = row[1]
-    def _get_total_words_for_track(self, track_id):
+        c.execute("SELECT COUNT(DISTINCT(track_id)) from lyrics")
+        self.totaldocs = float(c.fetchone()[0])
+
+    def calc_tfidf(self, track_id):
+
+        tfidf = []
+        words_in_song = []
+        total_words = 0
+
+        # get all words in a given song
         c = self.dbh.cursor()
-        c.execute("SELECT SUM(count) FROM lyrics WHERE track_id = ?", (track_id,))
-    def calc_tdidf(self, track_id, word):
-        pass
+        for row in c.execute("SELECT word, count FROM lyrics WHERE track_id = ?", (track_id,)):
+            words_in_song.append(row)
+            total_words += row[1]
+        total_words = float(total_words)
+        # calculate tfidf
+        for word,count in words_in_song:
+            tf = count / total_words
+            idf = log ( self.totaldocs / self.words[word] )
+            tfidf.append((word,tf * idf))
+
+        return tfidf
 
 def init_output_db(dbh):
-    # create the tdidf table
+    # create the tfidf table
     c = dbh.cursor()
-    c.execute('''CREATE TABLE tdidf
+    c.execute('''CREATE TABLE tfidf
               (track_id text,
                word text,
-               tdidf real)''')
+               tfidf real)''')
     dbh.commit()
 
 
-def main(input_db="mxm_dataset.db", output_db="mxm_tdidf.db"):
+def main(input_db="mxm_dataset.db", output_db="mxm_tfidf.db"):
     # load the databases
     mxm = sqlite3.connect(input_db)
     out = sqlite3.connect(output_db)
 
+    dbg("Creating output tables in {}".format(output_db))
     init_output_db(out) 
+    tdc = TFIDFCounter(mxm)
 
+    # calculate the tfidf for all documents
+    dbg("Begin calculating TFIDF...")
+    compl = 0
+    tenpcnt = int(tdc.totaldocs / 100)
+    c = mxm.cursor()
+    d = out.cursor()
+    query = "INSERT INTO tfidf VALUES ( ?, ?, ? )"
+    for row in c.execute ("SELECT DISTINCT(track_id) FROM lyrics"):
+        track_id = row[0]
+        for word, tfidf in tdc.calc_tfidf(track_id):
+            d.execute(query, (track_id, word, tfidf))
+        compl += 1
+        if (compl % tenpcnt == 0):
+            print ("{:.2f}% complete".format(compl / tdc.totaldocs * 100))
+    out.commit()
+    print ("Processed {} tracks".format(compl))
 
+def dbg(message):
+    sys.stderr.write(message + "\n")
 
 if __name__ == "__main__":
-    #main()
-    db = sqlite3.connect("/home/masen/mxm/rd/mxm_dataset.db")
-    tdc = TDIDFCounter(db)
-    tdc._init_totals_for_words()
-    print(tdc.words)
-
+    main(input_db="minidb.db")
